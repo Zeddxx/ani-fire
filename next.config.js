6d1c9a058ad2e5443f638b5935df84f25cc6794e
Sahil Ahmed <119150520+Zeddxx@users.noreply.github.com>
2024-01-29 15:27:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,9 +8,9 @@ const nextConfig = {
       },
     ],
   },
-  // experimental: {
-  //   missingSuspenseWithCSRBailout: false,
-  // },
+  experimental: {
+  missingSuspenseWithCSRBailout: false,
+ },
 };
 
 module.exports = nextConfig;
