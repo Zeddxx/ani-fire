be29d02790fe709cdb70a6c99be2a5bb81576674
Sahil Ahmed <zehraankhan7860@gmail.com>
2024-02-10 14:33:11 +0530

    "fix: fixed video height and width."

--- components/players/fire-player.tsx
+++ components/players/fire-player.tsx
@@ -61,7 +61,7 @@ const FirePlayer = ({ episodeId, server, category, poster }: VideoPlayerProps) =
     autoplay: false,
     autoOrientation: true,
     pip: true,
-    autoSize: true,
+    autoSize: false,
     fastForward: true,
     autoMini: false,
     screenshot: true,
@@ -108,7 +108,7 @@ const FirePlayer = ({ episodeId, server, category, poster }: VideoPlayerProps) =
 
   if (isLoading && !setUrl) {
     return(
-        <div className="xl:h-[460px] sm:h-96 h-64 w-full grid place-items-center">
+        <div className="aspect-video w-full grid place-items-center">
             <Loader2 className="animate-spin h-6 w-6" />
         </div>
     );
@@ -116,7 +116,7 @@ const FirePlayer = ({ episodeId, server, category, poster }: VideoPlayerProps) =
   return <Player
   option={options}
   subtitles={subtitles}
-  className="-z-10 art-container lg:max-h-[468px] h-[60vw] md:max-h-[630px] sm:max-h-[470px] max-h-[356px] w-full"
+  className="-z-10 art-container aspect-video"
   />;
 };
 export default FirePlayer;
