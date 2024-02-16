be3ea27cdb283a7736f72aa1262d219cc8ec7ebc
Sahil Ahmed <zehraankhan7860@gmail.com>
2024-02-16 11:38:12 +0530

    "bug: fixed."

--- actions/register.ts
+++ actions/register.ts
@@ -1,25 +1,25 @@
-'use server'
+"use server"
 
-import bcrypt from 'bcryptjs';
-import { RegisterSchema } from "@/lib/validation";
 import * as z from "zod"
-import { getUserByEmail } from '@/data/user';
-import { db } from '@/lib/db';
+import bcrypt from "bcryptjs"
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
+import { RegisterSchema } from "@/lib/validation";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
 
     if(!validatedFields.success) {
-        return { error: "Invalid Fields!" };
+        return { error: "Invalid fields!" };
     }
 
     const { email, password, name } = validatedFields.data;
     const hashedPassword = await bcrypt.hash(password, 10)
 
-    const existingUser = await getUserByEmail(email);
+    const existingUser = await getUserByEmail(email)
 
     if(existingUser) {
-        return { error: "Email already in use! 💀" };
+        return { error: "Email already in use! 😟" };
     }
 
     await db.user.create({
@@ -30,5 +30,5 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         }
     })
 
-    return { success: "User created successfully!" }
+    return { success: "Confirmation email sent!"}
 }
