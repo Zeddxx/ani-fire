7348cda2674b7401c9986f3c7d0d986105624016
Sahil Ahmed <zehraankhan7860@gmail.com>
2024-02-29 21:29:16 +0530

    "changes: changes the way of using the query-client."

--- lib/query-provider.tsx
+++ lib/query-provider.tsx
@@ -2,10 +2,12 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import React from "react";
 
-const queryClient = new QueryClient()
+// const queryClient = new QueryClient()
 
 export const QueryProvider = ({ children } :{ children: React.ReactNode }) => {
+    const [queryClient] = React.useState(() => new QueryClient())
     return(
         <QueryClientProvider client={queryClient}>
             {children}
