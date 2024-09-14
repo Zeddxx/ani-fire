f40e26b40fdd309f31ba62e7f108e6d03d427e02
sahil <zeddxxdev@gmail.com>
2024-09-14 22:13:17 +0530

    feat: refactoring marketing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,16 +1,18 @@
 export default function Marketing() {
   return (
     <main className="w-full h-auto min-h-lvh relative">
-      <header className="h-16 w-full wrapper-container sticky top-0 z-40 bg-black"></header>
+      <header className="h-16 w-full sticky top-0 z-40 bg-black">
+        <div className="flex justify-between items-center wrapper-container bg-white h-full"></div>
+      </header>
       <div className="h-full select-none w-full absolute inset-0 [mask-image:linear-gradient(to_top,transparent_10%,white_90%)]">
-        <video
+        {/* <video
           loop
           autoPlay
           muted
           className="max-h-[calc(100dvh-90px)] h-[100vw] min-h-[70dvh] w-full object-cover brightness-50"
         >
           <source src="/assets/video/videoplayback.mp4" type="video/mp4" />
-        </video>
+        </video> */}
       </div>
     </main>
   );
